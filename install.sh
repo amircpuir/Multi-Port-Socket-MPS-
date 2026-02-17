4f6eaaaa6871b5b916e7f53b5b7258b75ea16e47
amircpuir <amirkalaf542@gmail.com>
2026-02-17 06:38:26 +0330

    Modify install.sh to improve MPS session handling
    Updated installation script to run MPS in a detached screen session and added a command to attach to the session.

--- install.sh
+++ install.sh
@@ -1,18 +1,12 @@
-cat << 'EOF' > install.sh
 #!/bin/bash
 
 sudo apt update && sudo apt install screen wget -y
 
-
 wget -O mps.py https://raw.githubusercontent.com/amircpuir/Multi-Port-Socket-MPS-/main/mps.py
 
-
 echo -e "\033[92m[+] Installation Complete!\033[0m"
-echo -e "\033[93m[!] Starting MPS in a new Screen session...\033[0m"
+echo -e "\033[93m[!] Starting MPS inside Screen session 'mps_tunnel'...\033[0m"
 sleep 2
 
-screen -S mps_tunnel python3 mps.py
-
-EOF
-
-chmod +x install.sh
+screen -dmS mps_tunnel python3 mps.py
+screen -r mps_tunnel
